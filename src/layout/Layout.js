2b12b70aadd58375b687529cc05a6b9e3d9aa5e8
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-09 15:38:39 -0500

    ADD setup para el proyecto

---
+++ src/layout/Layout.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+          {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
