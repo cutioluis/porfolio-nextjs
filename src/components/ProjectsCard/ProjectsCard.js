267c9799d50043ef026f22a4222725ed03ce45e0
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-10 22:09:24 -0500

    FIX projects card section

--- src/components/ProjectsCard/ProjectsCard.js
+++ src/components/ProjectsCard/ProjectsCard.js
@@ -1,19 +1,31 @@
 import styles from "./ProjectsCard.module.css";
+import Image from "next/image";
+import homePic from '../../assets/img/luis-home.png'
 
 const ProjectsCard = () => {
   return (
     <section className={styles.ProjectsCard}>
       <article className={styles.projects_card}>
         <div className={styles.projects_card__container}>
-          <div className={styles.projects_card__cube}></div>
+          <div className={styles.projects_card__cube}>
+            <Image
+              className={styles.about__cube}
+              src={homePic}
+              alt="Project"
+              width={300}
+              height={200}
+              responsive
+            />
+          </div>
           <div className={styles.projects_card__description}>
             <h3>Porfolio web </h3>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque eros, turpis morbi mi praesent. Sit blandit vulputate proin 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Scelerisque eros, turpis
             </p>
             <div className={styles.projects_card_code}>
-                <a href="">Demo</a>
-                <a href="">Code</a>
+              <a href="">Demo</a>
+              <a href="">Code</a>
             </div>
           </div>
         </div>
