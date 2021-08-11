2fdec795d18843bcfac2f37c82405b80ec60189b
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-08-11 13:49:32 -0500

    FIX images and change to .svg

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
     {
       title: 'Seplom',
       description:"Seplom es una empresa de Plomeria Inmediata que brinda servicios de todo tipo para ayudar a personas.",
-      image: '/images/luis-home2.png',
+      image: '/img/luis-home2.svg',
       tags: ['React', 'JavaScript'],
       source: 'https://google.com',
       visit: 'https://google.com',
@@ -11,7 +11,7 @@ export const projects = [
     {
       title: 'Code Clean',
       description: "Es un servicio donde se permite la compra y venta de libros ofreciendo una gran experiencia de compra y venta.",
-        image: '/images/luis-home3.png',
+        image: '/img/luis-home3.svg',
         tags: ['React', 'JavaScript', 'Node', 'API REST'],
       source: 'https://google.com',
       visit: 'https://google.com',
@@ -20,7 +20,7 @@ export const projects = [
     {
       title: 'Frontend Cafe',
       description: "Una comunidad increible donde puedes proponer platicas y obtener tu propio profesor para clases online donde aprenderas de los mejores.",
-      image: '/images/4.jpg',
+      image: '/img/luis-home4.svg',
       tags: ['React', 'Api', 'Firebase'],
       source: 'https://google.com',
       visit: 'https://google.com',
