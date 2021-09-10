947e0180e14800f5ed666bf45d3422be61cb66fa
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-09-10 14:00:51 -0500

    FIX document bug

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import React from 'react'
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
